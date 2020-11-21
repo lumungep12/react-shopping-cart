5485a5d624bfead623fd668b17bc576c4ff0e00d
Erick Lumunge <lumungep12@gmail.com>
2020-11-21 11:01:09 +0300

    Product Component Update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 //Frature One
 import React from 'react';
+import Products from './Components/Products';
+import data from './data.json';
 
-export default function  App() {
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products : data.products,
+      size: "",
+      sort: "",
+    };
+  }
+
+
+  render(){
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="mainContent">
+            <Products products={this.state.products} />
+          </div>
+          <div className="sideBar">
+            Cart Items
+          </div>
+        </div>
       </main>
       <footer>
         All rights are Reserved
       </footer>
     </div>
   );
+  }
 }
+
+export default App;
