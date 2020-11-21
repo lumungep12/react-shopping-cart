d5e94a77226284378b5b25f5e21cca66111def4c
Erick Lumunge <lumungep12@gmail.com>
2020-11-21 15:48:13 +0300

    Filter Component update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //Frature One
 import React from 'react';
+import Filter from './Components/Filter';
 import Products from './Components/Products';
 import data from './data.json';
 
@@ -14,6 +15,37 @@ class App extends React.Component {
     };
   }
 
+  sortProducts = (e) => {
+    const sort = e.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products.slice().sort((a, b) => 
+      sort === "lowest" ?
+      a.price > b.price ?
+      1 : -1:
+      sort === "highest" ?
+      a.price < b.price ?
+      1 : -1:
+      a._id > b._id ?
+      1 : -1 ),
+    }));
+  };
+
+  filterProducts = (e) => {
+
+    if(e.target.value === ""){
+      this.setState({
+        size: e.target.value,
+        product: data.products
+      });
+    }else{
+    this.setState({
+      size: e.target.value,
+      products: data.products.filter(product => product.availableSizes.indexOf(e.target.value) >= 0)
+    });
+  }
+  }
+
 
   render(){
   return (
@@ -24,6 +56,13 @@ class App extends React.Component {
       <main>
         <div className="content">
           <div className="mainContent">
+            <Filter count={this.state.products.length}
+                    size = {this.state.size}
+                    sort = {this.state.sort} 
+                    
+                    filterProducts = {this.filterProducts} 
+                    sortProducts = {this.sortProducts}
+             />
             <Products products={this.state.products} />
           </div>
           <div className="sideBar">
