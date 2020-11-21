52dbd0fc2efc76d58b7272a741dab725c779bea6
Erick Lumunge <lumungep12@gmail.com>
2020-11-21 10:02:10 +0300

    Added source file

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function  App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All rights are Reserved
+      </footer>
+    </div>
+  );
+}
