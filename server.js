043f2a9a23e8e84e124b48a9addb70907e2153d8
Erick Lumunge <lumungep12@gmail.com>
2020-11-26 08:59:13 +0300

    Backend for Products

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const shortId = require('shortid');
+
+const app = express();
+app.use(bodyParser.json());
+// Initialize mongoose database
+mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+// Define product model
+const Product = mongoose.model("products", new mongoose.Schema({
+    _id: {type: String, default:shortId.generate},
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    availableSizes: [String],
+}))
+
+// Define first endpoint Getting list of products
+app.get("/api/products", async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+});
+
+// Creating new product in db
+app.post("/api/products", async (req, res) => {
+    const newProduct = new Product(req.body);
+    const savedProduct = await newProduct.save();
+    res.send(savedProduct)
+});
+
+//Deleting products
+app.delete("/api/products/:id", async (req, res) => {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    res.send(deletedProduct);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log("Server Running!! @ http://localhost:5000" )); 
